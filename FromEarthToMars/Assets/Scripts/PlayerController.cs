1cd95279908f924e26ab98f0166afeda94ced358
Chagrins <malcolmjgruber@gmail.com>
2018-09-29 15:25:35 -0500

    Map with tiles!

--- FromEarthToMars/Assets/Scripts/PlayerController.cs
+++ FromEarthToMars/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ namespace Chagrins
     {
         public float speed;
         public ContactFilter2D contactFilter;
+        public ContactFilter2D pointOfInterestFilter;
         public float colliderError = 0.001f;
 
         Vector3 velocity = Vector3.zero;
@@ -48,16 +49,21 @@ namespace Chagrins
                 tempVelocity.y -= speed;
             }
 
+            // Collision!
             float distance = speed * Time.deltaTime;
             Vector2 direction = tempVelocity.normalized;
             velocity = direction * distance;
 
             CheckLeftRight(direction.x, velocity.x);
             CheckUpDown(direction.y, velocity.y);
+            CheckPointOfInterests();
 
+            transform.position += velocity;
+
+            // Sprite stuff!!!
             if (Maths.EqualZero(direction.y) && !Maths.EqualZero(direction.x))
             {
-                if(Maths.EqualZero(animator.speed))
+                if (Maths.EqualZero(animator.speed))
                 {
                     animator.speed = 1f;
                 }
@@ -94,85 +100,90 @@ namespace Chagrins
                 animator.speed = 0f;
             }
 
-                transform.position += velocity;
         }
 
-    bool CheckLeftRight(float _xDirection, float _xSpeed)
-    {
-        bool impactCollision = false;
-        Vector2 newVelocity = Vector2.zero;
-
-        RaycastHit2D[] collisionResults = new RaycastHit2D[10];
-        Vector2 direction = new Vector2(_xDirection, 0f);
-
-        // General collision checks
-        int colliderCount = Physics2D.BoxCastNonAlloc(new Vector2(transform.position.x, transform.position.y) + collider2D.offset, boxCollider2D.size, 0f, direction, collisionResults, _xSpeed, contactFilter.layerMask);
-        Vector3 position = transform.position;
-
-        Vector2 leftOverTravel = Vector2.zero;
-        if (colliderCount > 0)
+        void CheckPointOfInterests()
         {
-            Debug.Log("LR Collider Count: " + colliderCount);
-            for (int colliderIndex = 0; colliderIndex < colliderCount; colliderIndex++)
+
+
+        }
+
+        bool CheckLeftRight(float _xDirection, float _xSpeed)
+        {
+            bool impactCollision = false;
+            Vector2 newVelocity = Vector2.zero;
+
+            RaycastHit2D[] collisionResults = new RaycastHit2D[10];
+            Vector2 direction = new Vector2(_xDirection, 0f);
+
+            // General collision checks
+            int colliderCount = Physics2D.BoxCastNonAlloc(new Vector2(transform.position.x, transform.position.y) + collider2D.offset, boxCollider2D.size, 0f, direction, collisionResults, _xSpeed, contactFilter.layerMask);
+            Vector3 position = transform.position;
+
+            Vector2 leftOverTravel = Vector2.zero;
+            if (colliderCount > 0)
             {
-                RaycastHit2D hit = collisionResults[colliderIndex];
-                if (!Maths.EqualZero(hit.normal.x))
+                Debug.Log("LR Collider Count: " + colliderCount);
+                for (int colliderIndex = 0; colliderIndex < colliderCount; colliderIndex++)
                 {
-                    float moveSign = Mathf.Sign(hit.normal.x);
-                    if (!Maths.EqualZero(_xSpeed) && (moveSign != Mathf.Sign(_xSpeed)))
+                    RaycastHit2D hit = collisionResults[colliderIndex];
+                    if (!Maths.EqualZero(hit.normal.x))
                     {
-                        velocity.x = 0;
-                        position.x = hit.point.x - (((boxCollider2D.size.x * 0.5f) + boxCollider2D.offset.x + colliderError) * -moveSign);
-                        impactCollision = true;
+                        float moveSign = Mathf.Sign(hit.normal.x);
+                        if (!Maths.EqualZero(_xSpeed) && (moveSign != Mathf.Sign(_xSpeed)))
+                        {
+                            velocity.x = 0;
+                            position.x = hit.point.x - (((boxCollider2D.size.x * 0.5f) + boxCollider2D.offset.x + colliderError) * -moveSign);
+                            impactCollision = true;
+                        }
                     }
                 }
             }
+
+            return impactCollision;
         }
 
-        return impactCollision;
-    }
-
-    bool CheckUpDown(float _yDirection, float _ySpeed)
-    {
-        bool impactCollision = false;
-        Vector2 newVelocity = Vector2.zero;
-
-        RaycastHit2D[] collisionResults = new RaycastHit2D[10];
-        Vector2 direction = new Vector2(0f, _yDirection);
-
-        // General collision checks
-        int colliderCount = Physics2D.BoxCastNonAlloc(new Vector2(transform.position.x, transform.position.y) + collider2D.offset, boxCollider2D.size, 0f, direction, collisionResults, _ySpeed, contactFilter.layerMask);
-        Vector3 position = transform.position;
-
-        Vector2 leftOverTravel = Vector2.zero;
-        if (colliderCount > 0)
+        bool CheckUpDown(float _yDirection, float _ySpeed)
         {
-            Debug.Log("UD Collider Count: " + colliderCount);
-            for (int colliderIndex = 0; colliderIndex < colliderCount; colliderIndex++)
+            bool impactCollision = false;
+            Vector2 newVelocity = Vector2.zero;
+
+            RaycastHit2D[] collisionResults = new RaycastHit2D[10];
+            Vector2 direction = new Vector2(0f, _yDirection);
+
+            // General collision checks
+            int colliderCount = Physics2D.BoxCastNonAlloc(new Vector2(transform.position.x, transform.position.y) + collider2D.offset, boxCollider2D.size, 0f, direction, collisionResults, _ySpeed, contactFilter.layerMask);
+            Vector3 position = transform.position;
+
+            Vector2 leftOverTravel = Vector2.zero;
+            if (colliderCount > 0)
             {
-                RaycastHit2D hit = collisionResults[colliderIndex];
-                if (!Maths.EqualZero(hit.normal.y))
+                Debug.Log("UD Collider Count: " + colliderCount);
+                for (int colliderIndex = 0; colliderIndex < colliderCount; colliderIndex++)
                 {
-                    float moveSign = Mathf.Sign(hit.normal.y);
-                    if (!Maths.EqualZero(_ySpeed) && (moveSign != Mathf.Sign(_ySpeed)))
+                    RaycastHit2D hit = collisionResults[colliderIndex];
+                    if (!Maths.EqualZero(hit.normal.y))
                     {
-                        velocity.y = 0;
-                        position.y = hit.point.y - (((boxCollider2D.size.y * 0.5f) + boxCollider2D.offset.y + colliderError) * -moveSign);
-                        impactCollision = true;
+                        float moveSign = Mathf.Sign(hit.normal.y);
+                        if (!Maths.EqualZero(_ySpeed) && (moveSign != Mathf.Sign(_ySpeed)))
+                        {
+                            velocity.y = 0;
+                            position.y = hit.point.y - (((boxCollider2D.size.y * 0.5f) + boxCollider2D.offset.y + colliderError) * -moveSign);
+                            impactCollision = true;
+                        }
                     }
                 }
             }
+
+            return impactCollision;
         }
 
-        return impactCollision;
-    }
+        // Overridable to make adjustments to the reaction
+        protected virtual bool HandleCollision(RaycastHit2D _hit)
+        {
+            bool collisionHappened = true;
 
-    // Overridable to make adjustments to the reaction
-    protected virtual bool HandleCollision(RaycastHit2D _hit)
-    {
-        bool collisionHappened = true;
-
-        return collisionHappened;
+            return collisionHappened;
+        }
     }
-}
 }
--- FromEarthToMars/Assets/TemplateAssets/ScoreDisplay.cs
+++ FromEarthToMars/Assets/TemplateAssets/ScoreDisplay.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScoreDisplay : MonoBehaviour {
+public class ScoreDisplay : MonoBehaviour
+{
 
     public Player player;
     private Text text;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         text = GetComponent<Text>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         text.text = string.Format("{0}Up\n{1}", (int)player.playerIndex, player.score.ToString("D6"));
-	}
+    }
 }
