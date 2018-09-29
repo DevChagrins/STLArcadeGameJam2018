bd83c711d802cf4a65b0d492030639916b27aec7
Chagrins <malcolmjgruber@gmail.com>
2018-09-29 11:14:38 -0500

    Fixed collision, allows player to slide against walls.

--- FromEarthToMars/Assets/Scripts/PlayerController.cs
+++ FromEarthToMars/Assets/Scripts/PlayerController.cs
@@ -52,32 +52,42 @@ namespace Chagrins
             Vector2 direction = tempVelocity.normalized;
             velocity = direction * distance;
 
-            CheckCollision(new Vector2(direction.x, 0f), velocity.x);
-            CheckCollision(new Vector2(0f, direction.y), velocity.y);
+            //CheckCollision(new Vector2(direction.x, 0f), velocity.x);
+            //CheckCollision(new Vector2(0f, direction.y), velocity.y);
+            CheckLeftRight(direction.x, velocity.x);
+            CheckUpDown(direction.y, velocity.y);
 
             transform.position += velocity;
         }
 
-        bool CheckCollision(Vector2 _direction, float _distance)
+        bool CheckLeftRight(float _xDirection, float _xSpeed)
         {
             bool impactCollision = false;
             Vector2 newVelocity = Vector2.zero;
 
             RaycastHit2D[] collisionResults = new RaycastHit2D[10];
+            Vector2 direction = new Vector2(_xDirection, 0f);
 
             // General collision checks
-            int colliderCount = Physics2D.BoxCastNonAlloc(new Vector2(transform.position.x, transform.position.y) + collider2D.offset, boxCollider2D.size, 0f, _direction, collisionResults, _distance, contactFilter.layerMask);
-            //int colliderCount = collider2D.Cast(_direction, contactFilter, collisionResults, _distance);
+            int colliderCount = Physics2D.BoxCastNonAlloc(new Vector2(transform.position.x, transform.position.y) + collider2D.offset, boxCollider2D.size, 0f, direction, collisionResults, _xSpeed, contactFilter.layerMask);
+            Vector3 position = transform.position;
 
             Vector2 leftOverTravel = Vector2.zero;
-            if(colliderCount > 0)
+            if (colliderCount > 0)
             {
-                Debug.Log("Collider Count: " + colliderCount);
-                for(int colliderIndex = 0; colliderIndex < colliderCount; colliderIndex++)
+                Debug.Log("LR Collider Count: " + colliderCount);
+                for (int colliderIndex = 0; colliderIndex < colliderCount; colliderIndex++)
                 {
-                    if(HandleCollision(collisionResults[colliderIndex]))
+                    RaycastHit2D hit = collisionResults[colliderIndex];
+                    if (!Maths.EqualZero(hit.normal.x))
                     {
-                        impactCollision = true;
+                        float moveSign = Mathf.Sign(hit.normal.x);
+                        if (!Maths.EqualZero(_xSpeed) && (moveSign != Mathf.Sign(_xSpeed)))
+                        {
+                            velocity.x = 0;
+                            position.x = hit.point.x - (((boxCollider2D.size.x * 0.5f) + boxCollider2D.offset.x + colliderError) * -moveSign);
+                            impactCollision = true;
+                        }
                     }
                 }
             }
@@ -85,6 +95,41 @@ namespace Chagrins
             return impactCollision;
         }
 
+        bool CheckUpDown(float _yDirection, float _ySpeed)
+        {
+            bool impactCollision = false;
+            Vector2 newVelocity = Vector2.zero;
+
+            RaycastHit2D[] collisionResults = new RaycastHit2D[10];
+            Vector2 direction = new Vector2(0f, _yDirection);
+
+            // General collision checks
+            int colliderCount = Physics2D.BoxCastNonAlloc(new Vector2(transform.position.x, transform.position.y) + collider2D.offset, boxCollider2D.size, 0f, direction, collisionResults, _ySpeed, contactFilter.layerMask);
+            Vector3 position = transform.position;
+
+            Vector2 leftOverTravel = Vector2.zero;
+            if (colliderCount > 0)
+            {
+                Debug.Log("UD Collider Count: " + colliderCount);
+                for (int colliderIndex = 0; colliderIndex < colliderCount; colliderIndex++)
+                {
+                    RaycastHit2D hit = collisionResults[colliderIndex];
+                    if (!Maths.EqualZero(hit.normal.y))
+                    {
+                        float moveSign = Mathf.Sign(hit.normal.y);
+                        if (!Maths.EqualZero(_ySpeed) && (moveSign != Mathf.Sign(_ySpeed)))
+                        {
+                            velocity.y = 0;
+                            position.y = hit.point.y - (((boxCollider2D.size.y * 0.5f) + boxCollider2D.offset.y + colliderError) * -moveSign);
+                            impactCollision = true;
+                        }
+                    }
+                }
+            }
+
+            return impactCollision;
+        }
+        
         // Overridable to make adjustments to the reaction
         protected virtual bool HandleCollision(RaycastHit2D _hit)
         {
