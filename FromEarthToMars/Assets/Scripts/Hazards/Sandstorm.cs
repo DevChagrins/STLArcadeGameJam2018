b1ed88265974e742d64817767115e81af0acd687
Chagrins <malcolmjgruber@gmail.com>
2018-09-29 18:14:34 -0500

    Merged

--- FromEarthToMars/Assets/Scripts/Hazards/Sandstorm.cs
+++ FromEarthToMars/Assets/Scripts/Hazards/Sandstorm.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Sandstorm : MonoBehaviour
 {
+    public GameObject LargeSandstorm = null;
+    public GameObject SmallSandstorm = null;
+    public int largeStormCount = 5;
+    public int smallStormCount = 8;
 
     // Use this for initialization
     void Start()
     {
+        for(int largeCount = 0; largeCount < largeStormCount; largeCount++)
+        {
 
+        }
     }
 
     // Update is called once per frame
