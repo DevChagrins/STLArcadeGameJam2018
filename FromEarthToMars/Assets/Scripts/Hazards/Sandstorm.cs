c9f22a6f64df1f7383624e73fa042efb7cef9cc5
Chagrins <malcolmjgruber@gmail.com>
2018-09-29 18:08:44 -0500

    Fixed player prefab

--- FromEarthToMars/Assets/Scripts/Hazards/Sandstorm.cs
+++ FromEarthToMars/Assets/Scripts/Hazards/Sandstorm.cs
@@ -2,20 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotator : MonoBehaviour
+public class Sandstorm : MonoBehaviour
 {
-    public Vector3 rotationSpeeds;
-    public Vector3 startRotation;
 
     // Use this for initialization
     void Start()
     {
-        transform.Rotate(startRotation);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(rotationSpeeds * Time.deltaTime);
+
     }
 }
