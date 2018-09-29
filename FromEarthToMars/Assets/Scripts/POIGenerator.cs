75b6883214e578a36a1c103658a7cf34df2ad60f
Nicholas DeTore <Nicholas DeTore>
2018-09-29 16:16:29 -0500

    point of interest

---
+++ FromEarthToMars/Assets/Scripts/POIGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class POIGenerator : MonoBehaviour
+{
+    public GameObject POIPrefab = null;
+    public Transform startPoint;
+    public Transform endPoint;
+
+
+    void Start()
+    {
+        float positionX = startPoint.position.x;
+        float positionY = startPoint.position.y;
+
+        
+        GameObject POI = GameObject.Instantiate(POIPrefab, new Vector3(positionX, positionY, 0f), Quaternion.identity);
+        
+    }
+
+    void Update()
+    {
+
+    }
+}
