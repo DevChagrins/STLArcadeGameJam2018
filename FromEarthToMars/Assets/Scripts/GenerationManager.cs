25cb13bc560b8f318dffdb0f92f61b6b2801cdab
Chagrins <malcolmjgruber@gmail.com>
2018-09-30 14:56:21 -0500

    Menu stuff.

---
+++ FromEarthToMars/Assets/Scripts/GenerationManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerationManager : MonoBehaviour
+{
+    public POIGenerator poiGenScript;
+    public SandstormGenerator sandGenScript;
+    public SimpleCameraScroll cameraScrollScript;
+    public ArrowManager arrowManScript;
+    public GameObject cloudParticles;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void EnableGeneration()
+    {
+        poiGenScript.enabled = true;
+        sandGenScript.enabled = true;
+        cameraScrollScript.enabled = true;
+        arrowManScript.enabled = true;
+        cloudParticles.SetActive(true);
+    }
+
+    public void DisableGeneration()
+    {
+        poiGenScript.enabled = false;
+        sandGenScript.enabled = false;
+        cameraScrollScript.enabled = false;
+        arrowManScript.enabled = false;
+        cloudParticles.SetActive(false);
+    }
+}
---
+++ FromEarthToMars/Assets/Scripts/MainMenu/MenuNavigator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuNavigator : MonoBehaviour
+{
+    public enum MenuState
+    {
+        MAIN,
+        HOWTO,
+        RESULTS,
+        GAME,
+    }
+
+    public MenuState menuState = 0;
+
+    public GameObject mainScreen = null;
+    public GameObject howToScreen = null;
+    public GameObject resultsScreen = null;
+    public GameObject gameScreen = null;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GoToResults()
+    {
+        mainScreen.SetActive(false);
+        howToScreen.SetActive(false);
+        resultsScreen.SetActive(true);
+        mainScreen.SetActive(false);
+    }
+}
