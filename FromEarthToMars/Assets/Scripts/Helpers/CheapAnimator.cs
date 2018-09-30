c1344580500594cd7c3b7c08abb1780fcb721bca
Nicholas DeTore <Nicholas DeTore>
2018-09-29 19:26:01 -0500

    added rover, earth, sparkle animations

--- FromEarthToMars/Assets/Scripts/Helpers/CheapAnimator.cs
+++ FromEarthToMars/Assets/Scripts/Helpers/CheapAnimator.cs
@@ -6,18 +6,48 @@ using UnityEngine;
 // TO DO: FILL OUT LATER!!
 public class CheapAnimator : MonoBehaviour
 {
+    public UnityEngine.UI.Image spriteRenderer;
     public Sprite[] spriteFrames;
     public float animationSpeed;
+    private float animationTimer;
+    private int currentFrame;
 
     // Use this for initialization
     void Start()
     {
+        animationTimer = animationSpeed;
+        spriteRenderer = GetComponent<UnityEngine.UI.Image>();
+        currentFrame = 0;
 
+        if (spriteRenderer)
+        {
+            spriteRenderer.sprite = spriteFrames[currentFrame];
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (animationTimer > 0f)
+        {
+            animationTimer -= Time.deltaTime;
+        }
 
+        if (animationTimer <= 0f)
+        {
+            animationTimer += animationSpeed;
+            currentFrame++;
+
+            if (currentFrame >= spriteFrames.Length)
+            {
+                currentFrame = 0;
+            }
+            else if (currentFrame < 0)
+            {
+                currentFrame = spriteFrames.Length - 1;
+            }
+
+            spriteRenderer.sprite = spriteFrames[currentFrame];
+        }
     }
 }
--- FromEarthToMars/Assets/Scripts/POIGenerator.cs
+++ FromEarthToMars/Assets/Scripts/POIGenerator.cs
@@ -8,26 +8,35 @@ public class POIGenerator : MonoBehaviour
     public Transform startPoint;
     public Transform endPoint;
     public int instanceCount = 15;
+    public float minRange = 2.56f;
+    public float maxRange = 10.24f;
 
     void Start()
     {
         Vector2 startPos = startPoint.position;
-        Vector2 endPos = new Vector2(startPoint.position.x + Random.Range(50, 50), endPoint.position.y);
+        Vector2 endPos = new Vector2(startPoint.position.x + Random.Range(minRange, maxRange), endPoint.position.y);
 
-        for(int i = 0; i< instanceCount; i++){
+        for(int i = 0; i< instanceCount; i++) {
             GameObject lastGeneratedPOI = generatePoint(startPos, endPos);
+            startPos = lastGeneratedPOI.transform.position;
+            startPos.y = startPoint.position.y;
+            endPos = new Vector2(startPos.x + Random.Range(minRange, maxRange), endPoint.position.y);
+            if((endPos.x > endPoint.position.x) || (endPoint.position.x - endPos.x <= 1.28))
+                break;
+            
         }
         
     }
 
     GameObject generatePoint(Vector2 Start, Vector2 End) 
     {
-        float positionX = startPoint.position.x;
-        float positionY = startPoint.position.y;
+        float positionX = Start.x;
+        float positionY = Start.y;
         float ranW = Random.Range(0f, 359.9f);
 
-        positionX = Random.Range(startPoint.position.x, endPoint.position.x);
-        positionY = Random.Range(startPoint.position.y, endPoint.position.y);
+        //positionX = Random.Range(Start.x, End.x);
+        positionX = End.x;
+        positionY = Random.Range(Start.y, End.y);
         GameObject POI = GameObject.Instantiate(POIPrefab, new Vector3(positionX, positionY, 0f), Quaternion.identity);
 
         // Set random rotation on POI object
