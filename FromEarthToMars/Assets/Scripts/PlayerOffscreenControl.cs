989360e84638e58f9697fcb1bdf87eae3acb61fa
DrDoak <larryzhao740@yahoo.com>
2018-09-30 13:01:45 -0500

    Added out of world prefab

---
+++ FromEarthToMars/Assets/Scripts/PlayerOffscreenControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chagrins {
+	public class PlayerOffscreenControl : MonoBehaviour {
+
+		public PlayerController pc;
+		public float ScrollSpeed = 0.1f;
+		public Vector2 MinCoord;
+		public Vector2 MaxCoord;
+		public Vector2 CenterCoord;
+		public float xOff = 0f;
+		public float ImageOffset = 1.5f;
+
+		private bool ImageOn = false;
+
+		public Vector3 PPos;
+
+		public GameObject ChildArrow;
+		// Use this for initialization
+		void Start () {
+			if (pc == null)
+				pc = FindObjectOfType<PlayerController> ();
+			ChildArrow = transform.Find ("Arrow").gameObject;
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			xOff += (ScrollSpeed * Time.deltaTime);
+			if (pc != null) {
+				PPos = pc.transform.position;
+				ImageOn = (PPos.x < MinCoord.x + xOff || PPos.x > MaxCoord.x + xOff ||
+					PPos.y < MinCoord.y || PPos.y > MaxCoord.y);
+			}
+			UpdateImage (ImageOn);
+		}
+
+		void UpdateImage(bool On) {
+			float a = Mathf.Atan2 (pc.transform.position.y - CenterCoord.y, pc.transform.position.x - CenterCoord.x + xOff);
+			if (On)
+				transform.position = new Vector3 (xOff + CenterCoord.x + 1.5f * Mathf.Cos(a) , CenterCoord.y + 1.5f * Mathf.Sin(a), 0f);
+			else
+				transform.position = new Vector3(-10f,0f,0f);
+			ChildArrow.transform.localRotation = Quaternion.Euler(0f,0f, a * Mathf.Rad2Deg);
+		}
+	}
+}
