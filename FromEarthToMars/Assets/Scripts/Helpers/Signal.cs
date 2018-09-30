4a7ea12e2b47f5fb08dfd80e4f42c5516ae9a928
DrDoak <larryzhao740@yahoo.com>
2018-09-29 19:30:05 -0500

    Fixed movement stuck glitch

---
+++ FromEarthToMars/Assets/Scripts/Helpers/Signal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Signal : MonoBehaviour {
+
+	public float Speed;
+	public InputCommand mInput;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<TrailRenderer> ().time = mInput.duration;
+		transform.position = new Vector3(transform.position.x, transform.position.y + Speed, transform.position.z) ;
+		if (mInput.IsDead(Time.timeSinceLevelLoad))
+			Destroy (gameObject);
+	}
+}
