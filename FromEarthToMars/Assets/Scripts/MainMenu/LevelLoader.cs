e0c528decc92643109e29ea0de7cba6ec4adcd0a
Chagrins <malcolmjgruber@gmail.com>
2018-09-30 12:01:39 -0500

    Master sceene that loads the game.

---
+++ FromEarthToMars/Assets/Scripts/MainMenu/LevelLoader.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public string[] gameScenes;
+    private int sceneLoadedCount;
+    // Use this for initialization
+    void Start()
+    {
+        sceneLoadedCount = 0;
+        LoadGameScenes();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadGameScenes()
+    {
+        if(gameScenes.Length > 0)
+        {
+            foreach(string scene in gameScenes)
+            {
+                AsyncOperation loadOperation = SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+                loadOperation.completed += new System.Action<AsyncOperation>(SceneLoaded);
+            }
+        }
+    }
+
+    public void SceneLoaded(AsyncOperation _operation)
+    {
+        if(_operation.isDone)
+        {
+            sceneLoadedCount++;
+        }
+    }
+
+    public void SceneUnloaded(AsyncOperation _operation)
+    {
+        if (_operation.isDone)
+        {
+            sceneLoadedCount--;
+        }
+    }
+
+    public void UnloadGameScene()
+    {
+        if (gameScenes.Length > 0)
+        {
+            foreach (string scene in gameScenes)
+            {
+                AsyncOperation loadOperation = SceneManager.UnloadSceneAsync(scene);
+                loadOperation.completed += new System.Action<AsyncOperation>(SceneUnloaded);
+            }
+        }
+    }
+}
