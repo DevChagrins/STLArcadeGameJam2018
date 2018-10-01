97a2191df2a1d249b87638bdb04bd7759bf68c92
malcolmjgruber@gmail.com <malcolmjgruber@gmail.com>
2018-09-30 23:28:12 -0500

    Fixed some Winnitron problem

--- FromEarthToMars/Assets/Scripts/PlayerController.cs
+++ FromEarthToMars/Assets/Scripts/PlayerController.cs
@@ -8,113 +8,135 @@ namespace Chagrins
     [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerController : BaseController
     {
-		public bool Player1 = true;
+        public bool Player1 = true;
         public float speed;
         public ContactFilter2D contactFilter;
         public ContactFilter2D pointOfInterestFilter;
         public float colliderError = 0.001f;
-		public float inputDelay = 1.0f;
+        public float inputDelay = 1.0f;
         public float actionTime = 1.0f;
 
         Vector3 velocity = Vector3.zero;
-		bool firstCall = true;
-		private List<InputCommand> m_pendingInputs;
-		public InputCommand m_lastInput;
+        bool firstCall = true;
+        private List<InputCommand> m_pendingInputs;
+        public InputCommand m_lastInput;
 
-		private float m_pauseInputTime = 0.0f;
+        private float m_pauseInputTime = 0.0f;
         private CountDownTimer countDownTimer = null;
 
         void Start()
         {
             _Initialize();
-			m_pendingInputs = new List<InputCommand> ();
+            m_pendingInputs = new List<InputCommand>();
             countDownTimer = GameObject.FindGameObjectWithTag("Timer")?.GetComponent<CountDownTimer>();
         }
 
-        void Update() {	}
+        void Update() { }
 
         void FixedUpdate()
         {
             _Update();
         }
 
-		private void IssueInputs() {
-			Vector3 targetVel = new Vector3 ();
-			if (Player1) {
-				if (Input.GetKey (KeyCode.LeftArrow)) {
-					targetVel.x -= speed;
-				}
+        private void IssueInputs()
+        {
+            Vector3 targetVel = new Vector3();
+            if (Player1)
+            {
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    targetVel.x -= speed;
+                }
 
-				if (Input.GetKey (KeyCode.RightArrow)) {
-					targetVel.x += speed;
-				}
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    targetVel.x += speed;
+                }
 
-				if (Input.GetKey (KeyCode.UpArrow)) {
-					targetVel.y += speed;
-				}
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    targetVel.y += speed;
+                }
 
-				if ( Input.GetKey (KeyCode.DownArrow)) {
-					targetVel.y -= speed;
-				}
-			} else {
-				if (Input.GetKey (KeyCode.A) ) {
-					targetVel.x -= speed;
-				}
+                if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    targetVel.y -= speed;
+                }
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.A))
+                {
+                    targetVel.x -= speed;
+                }
 
-				if (Input.GetKey (KeyCode.D)) {
-					targetVel.x += speed;
-				}
+                if (Input.GetKey(KeyCode.D))
+                {
+                    targetVel.x += speed;
+                }
 
-				if (Input.GetKey (KeyCode.W)) {
-					targetVel.y += speed;
-				}
+                if (Input.GetKey(KeyCode.W))
+                {
+                    targetVel.y += speed;
+                }
 
-				if (Input.GetKey (KeyCode.S) ) {
-					targetVel.y -= speed;
-				}
-			}
-			if (targetVel != Vector3.zero) {
-				if (m_lastInput == null ||
-				    m_lastInput.direction != targetVel) {
-					InputCommand newCommand = new InputCommand ();
-					newCommand.ActiveTime = Time.timeSinceLevelLoad + inputDelay;
-					newCommand.duration = 0f;
-					newCommand.direction = targetVel;
-					newCommand.Delay = inputDelay;
-					m_pendingInputs.Add (newCommand);
-					m_lastInput = newCommand;
-					FindObjectOfType<ArrowManager> ().CreateArrow (newCommand,Player1);
-				} else {					
-					m_lastInput.duration += Time.fixedDeltaTime;
-				}
-			} else {
-				m_lastInput = null;
-			}
-		}
+                if (Input.GetKey(KeyCode.S))
+                {
+                    targetVel.y -= speed;
+                }
+            }
+            if (targetVel != Vector3.zero)
+            {
+                if (m_lastInput == null ||
+                    m_lastInput.direction != targetVel)
+                {
+                    InputCommand newCommand = new InputCommand();
+                    newCommand.ActiveTime = Time.timeSinceLevelLoad + inputDelay;
+                    newCommand.duration = 0f;
+                    newCommand.direction = targetVel;
+                    newCommand.Delay = inputDelay;
+                    m_pendingInputs.Add(newCommand);
+                    m_lastInput = newCommand;
+                    FindObjectOfType<ArrowManager>().CreateArrow(newCommand, Player1);
+                }
+                else
+                {
+                    m_lastInput.duration += Time.fixedDeltaTime;
+                }
+            }
+            else
+            {
+                m_lastInput = null;
+            }
+        }
 
-		private Vector3 calculateTempVelocity() {
-			List<InputCommand> m_nextInputs = new List<InputCommand> ();
-			Vector3 tempVelocity =  new Vector3();
-			foreach (InputCommand ic in m_pendingInputs) {
-				if (ic.IsActive (Time.timeSinceLevelLoad)) {
-					tempVelocity += ic.direction;
-				} 
-				if (!ic.IsDead(Time.timeSinceLevelLoad)) {
-					m_nextInputs.Add (ic);
-				}
-			}
-			m_pendingInputs = m_nextInputs;
-			return tempVelocity;
-		}
+        private Vector3 calculateTempVelocity()
+        {
+            List<InputCommand> m_nextInputs = new List<InputCommand>();
+            Vector3 tempVelocity = new Vector3();
+            foreach (InputCommand ic in m_pendingInputs)
+            {
+                if (ic.IsActive(Time.timeSinceLevelLoad))
+                {
+                    tempVelocity += ic.direction;
+                }
+                if (!ic.IsDead(Time.timeSinceLevelLoad))
+                {
+                    m_nextInputs.Add(ic);
+                }
+            }
+            m_pendingInputs = m_nextInputs;
+            return tempVelocity;
+        }
 
         protected override void _Step()
         {
-			if (m_pauseInputTime <= 0.0f)
-				IssueInputs ();
-			else
-				m_pauseInputTime -= Time.deltaTime;
-			Vector3 tempVelocity = calculateTempVelocity ();
-				
+            if (m_pauseInputTime <= 0.0f)
+                IssueInputs();
+            else
+                m_pauseInputTime -= Time.deltaTime;
+            Vector3 tempVelocity = calculateTempVelocity();
+
             // Collision!
             float distance = speed * Time.deltaTime;
             Vector2 direction = tempVelocity.normalized;
@@ -184,12 +206,12 @@ namespace Chagrins
         {
             PointOfInterest poi = collider.gameObject.GetComponent<PointOfInterest>();
 
-			if (poi == null)
-				return;
+            if (poi == null)
+                return;
             // Add time
             float? timeValue = poi?.GetTimeValue();
 
-            if(timeValue.HasValue)
+            if (timeValue.HasValue)
             {
                 //Debug.Log("Collecting!");
                 // Actually add the time to the overall counter
@@ -202,15 +224,16 @@ namespace Chagrins
             poi?.EnableSelfDestruction(actionTime);
         }
 
-		public void FreezeInput(float f, bool ClearCurrentInputs) {
-			if (m_pauseInputTime > 0f)
-				return;
-			m_pauseInputTime = f;
-			foreach (InputCommand i in m_pendingInputs)
-				i.duration = 0f;
-			if (ClearCurrentInputs)
-				m_pendingInputs.Clear ();
-		}
+        public void FreezeInput(float f, bool ClearCurrentInputs)
+        {
+            if (m_pauseInputTime > 0f)
+                return;
+            m_pauseInputTime = f;
+            foreach (InputCommand i in m_pendingInputs)
+                i.duration = 0f;
+            if (ClearCurrentInputs)
+                m_pendingInputs.Clear();
+        }
 
         bool CheckLeftRight(float _xDirection, float _xSpeed)
         {
@@ -230,7 +253,7 @@ namespace Chagrins
                 for (int colliderIndex = 0; colliderIndex < colliderCount; colliderIndex++)
                 {
                     RaycastHit2D hit = collisionResults[colliderIndex];
-					if (!hit.collider.isTrigger && !Maths.EqualZero(hit.normal.x))
+                    if (!hit.collider.isTrigger && !Maths.EqualZero(hit.normal.x))
                     {
                         float moveSign = Mathf.Sign(hit.normal.x);
                         if (!Maths.EqualZero(_xSpeed) && (moveSign != Mathf.Sign(_xSpeed)))
@@ -261,17 +284,17 @@ namespace Chagrins
             Vector2 leftOverTravel = Vector2.zero;
             if (colliderCount > 0)
             {
-				
+
                 for (int colliderIndex = 0; colliderIndex < colliderCount; colliderIndex++)
                 {
                     RaycastHit2D hit = collisionResults[colliderIndex];
-					//Debug.Log (hit.collider.gameObject);
-					if (!hit.collider.isTrigger && !Maths.EqualZero(hit.normal.y))
+                    //Debug.Log (hit.collider.gameObject);
+                    if (!hit.collider.isTrigger && !Maths.EqualZero(hit.normal.y))
                     {
                         float moveSign = Mathf.Sign(hit.normal.y);
                         if (!Maths.EqualZero(_ySpeed) && (moveSign != Mathf.Sign(_ySpeed)))
                         {
-							
+
                             velocity.y = 0;
                             position.y = hit.point.y - (((boxCollider2D.size.y * 0.5f) + boxCollider2D.offset.y + colliderError) * -moveSign);
                             impactCollision = true;
@@ -293,7 +316,7 @@ namespace Chagrins
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            if(collision.CompareTag("Hazard"))
+            if (collision.CompareTag("Hazard"))
             {
                 // Lose command here
             }
